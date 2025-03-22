39a2859d18f1eb50b8d468e5a559d726eb382ee3
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-03-23 01:29:45 +0530

    intital message

--- OnlineExamBackend/src/main/java/com/OnlineExam/OnlineExamApplication.java
+++ OnlineExamBackend/src/main/java/com/OnlineExam/OnlineExamApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineExamApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Online Exam Application...");
 		SpringApplication.run(OnlineExamApplication.class, args);
+		System.out.println("Application Started Successfully!");
 	}
 
 }
